51c6526e0f8519821887af225d12b595debcac55
Hiroki120301 <hxn137@case.edu>
2023-06-28 11:52:52 -0400

    terraform with docker

---
+++ gcp_terraform/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("credentials.json")
+
+  project = "terraform-on-gcp-hiroki"
+  region  = "us-central1"
+  zone    = "us-centrals1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
